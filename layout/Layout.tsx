1dca13d46e994a816de04ec18b6264715f26ef02
“Sakdipat <645021000593@mail.rmutk.ac.th>
2025-08-04 13:05:57 +0700

    วางโครง Layout

---
+++ layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import './styles/layout.css';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <div className='header'><Header /></div>
+            <div className='sidebar'><Sidebar /></div>
+            <h1></h1>
+            <div style={{ backgroundColor: "lightgray" }}>
+                {children}</div>
+        </div>
+    )
+}
+
+export default Layout
---
+++ layout/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/sidebar.css';
+
+const Sidebar = () => {
+  return (
+     <div className='sidebar'>
+          <span>♦</span>
+          <span>SOFTTHAI</span>
+    </div>
+  )
+}
+
+export default Sidebar
