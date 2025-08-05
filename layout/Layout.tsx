0ad117e8c13ed2d410cfdf01a51f19521aac2372
“Sakdipat <645021000593@mail.rmutk.ac.th>
2025-08-05 17:57:34 +0700

    Sidebar

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
-/*import Header from './Header'*/
 import Sidebar from './Sidebar'
+import Header from './Header';
 import './styles/layout.css';
 
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div>
-            {/*<div className='header'><Header /></div>*/}
-            <div className='sidebar'><Sidebar /></div>
-            <h1></h1>
-            <div style={{ backgroundColor: "lightgray" }}>
-                {children}</div>
-            <div className="sidebar-menu"><Sidebar /></div>
+    // return (
+    //     <div>
+    //         <div className='sidebar'><Sidebar /></div>
+    //         <h1></h1>
+    //          <div style={{ backgroundColor: "lightgray" }}>
+    //             {children}
+    //          </div>
+    //         <div className='header'>Header</div>
+    //         <div className='content'>content</div>
+    //     </div>
+    // )
+    return(
+        <div className='container'>
+            {/* ซ้าย/ */}
+            <div className='sidebar-container'><Sidebar /></div>
+            {/* ขวา/ */}
+            <div className='wapper'>
+                 {/* header/ */}
+                <div className='header'><Header /></div>
+                {/* content/ */}
+                <div className='content'>{children}</div>
+            </div>
         </div>
     )
 }
--- layout/Sidebar.tsx
+++ layout/Sidebar.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import './styles/sidebar.css';
+import Link from 'next/link';
+
+const menuItems = [
+  { name: "Dashboard", href: "/dashboard", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z" /></svg> },
+  { name: "News", href: "/news", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h440l200 200v440q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H600v-160H200v560Zm80-80h400v-80H280v80Zm0-320h200v-80H280v80Zm0 160h400v-80H280v80Zm-80-320v160-160 560-560Z"/></svg> },
+  { name: "Document", href: "/document", icon:<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M320-440h320v-80H320v80Zm0 120h320v-80H320v80Zm0 120h200v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg> },
+  { name: "User Role", href: "/user-role", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg> },
+  { name: "User Group", href: "/user-group", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg> },
+  { name: "User Permission", href: "/user-permission", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M702-480 560-622l57-56 85 85 170-170 56 57-226 226Zm-342 0q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 260Zm0-340Z"/></svg> },
+  { name: "Master Data", href: "/master-data", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M600-40q-33 0-56.5-23.5T520-120q0-23 11-41t29-29v-221q-18-11-29-28.5T520-480q0-33 23.5-56.5T600-560q33 0 56.5 23.5T680-480q0 23-11 40.5T640-411v115l160-53v-62q-18-11-29-28.5T760-480q0-33 23.5-56.5T840-560q33 0 56.5 23.5T920-480q0 23-11 40.5T880-411v119l-240 80v22q18 11 29 29t11 41q0 33-23.5 56.5T600-40ZM160-160v-560 560Zm0 0q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640H447l-80-80H160v480h280v80H160Z"/></svg> },
+  { name: "History Log", href: "/history-log", icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/></svg> },
+];
 
 const Sidebar = () => {
   return (
-    <>
-      <div className="sidebar">
-          ♦ Softthai 
-        <div className="sidebar-menu">
-          <div>Dashboard</div>
-          <div>News</div>
-          <div>Document</div>
-          <div>User Role</div>
-          <div>User Group</div>
-          <div>User Permission</div>
-          <div>Master Data</div>
-          <div>History Log</div>
-        </div>
+    <div className="sidebar">
+      <div>
+        <Link href="/home" className="custom-link">
+          ♦ Softthai
+        </Link>
       </div>
-    </>
-  )
-}
 
-export default Sidebar
+      <div className="sidebar-menu">
+        <div>Menu</div>
+      </div>
+
+      <div className="menu">
+        {menuItems.map((item, index) => (
+          <div key={index} className="menu-item">
+            <div className="menu-icon">
+              {item.icon}
+            </div>
+            <Link href={item.href} className="custom-link">
+              {item.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
